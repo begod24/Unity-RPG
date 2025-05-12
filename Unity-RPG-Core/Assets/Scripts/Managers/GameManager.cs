927841f3949ecf798dfd855ee560cb5703475182
Bekbolat Aldiyarov <bekbolat.aldiyarov@ci.global>
2025-05-12 09:43:02 +0200

    refactor
    all classes and methods

--- Unity-RPG-Core/Assets/Scripts/Managers/GameManager.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class GameManager : SingletonBehaviour<GameManager>
-{
-    public void LoadScene(string name) => SceneManager.LoadScene(name);
-    public void Quit() => Application.Quit();
-}
